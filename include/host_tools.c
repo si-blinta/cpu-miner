95fe312b79578f7f84e228e7366037aa0ffd1b20
si-blinta <nader.ben.ammar.pro@gmail.com>
2024-03-09 16:36:20 +0100

    added

--- include/host_tools.c
+++
@@ -1,9 +0,0 @@
-#include "host_tools.h"
-
-void HOST_TOOLS_send_id(struct dpu_set_t set){
-  struct dpu_set_t dpu;
-  uint32_t id = 0;
-  DPU_FOREACH(set,dpu,id){
-      DPU_ASSERT(dpu_copy_to(dpu, "dpu_id", 0,&id,sizeof(uint32_t)));
-  }
-}
